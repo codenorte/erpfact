e6ea41aa699849731de2223dc464abb56dfc910f
Marco Tocagon <mbtocagon@gmail.com>
2022-09-05 13:02:22 -0500

    origin

--- app/Adjustment.php
+++ app/Adjustment.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Adjustment extends Model
 {
     protected $fillable =[
-        "reference_no", "warehouse_id", "document", "total_qty", "item", 
+         "reference_no", "warehouse_id", "document", "total_qty", "item", 
          "note"   
     ];
 }
